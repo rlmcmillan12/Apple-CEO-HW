03233d6a029dc4b107d538633f228741cf290ddb
Ryan <rlmcmillan12@gmail.com>
2022-05-23 11:13:19 -0400

    worked on hw problems

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const http = require('http')
 const express = require('express')
 const es6Renderer = require('express-es6-template-engine')
-const data = require('./data')
-const { friends } = require('./data')
+const ceos = require('./data')
 
 
 const hostname = 'localhost'
@@ -23,3 +22,65 @@ const partials = {
 
 //public folder/ static files
 app.use(express.static('./public'))
+
+app.get('/', (req, res) => {
+    res.render('home', {
+        partials,
+        locals: {
+            title: `Apple CEO's Home`
+        }
+    })
+});
+
+app.get('/ceos', (req, res) => {
+    res.render('ceo-list', {
+        partials,
+        locals: {
+            ceos,
+            title: "CEO's"
+        }
+    })
+});
+
+app.get('/ceos/:slug', (req, res) => {
+    currentCEO = ceos.find(p => p.slug === req.params.slug)
+    if (!currentCEO) {
+        res.status(404).send(`Could not find CEO with slug: ${req.params.slug}`)
+        return
+    }
+    res.render('ceo-details', {
+        partials,
+        locals: {
+            currentCEO,
+            title: `${currentCEO.name}'s Details`
+        }
+    });
+});
+
+app.post('/ceos/:name', (req, res) => {
+    currentCEO = ceos.find(p => p.name.toLowerCase() === req.params.name.replace("+", " ").toLowerCase())
+    if (!currentCEO) {
+        res.status(404).send(`Could not find CEO with name: ${req.params.name}`)
+        return
+    }
+    res.render('ceo-details', {
+        partials,
+        locals: {
+            currentCEO,
+            title: `${currentCEO.name}'s Details`
+        }
+    });
+});
+
+//catch any missing pages
+app.get('*', (req, res) => {
+    res.send('Page not found')
+})
+
+
+
+
+//server listener
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`)
+})
